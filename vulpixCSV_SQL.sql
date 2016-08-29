68dcd0a9aaf9ad4974d2f3d15473ac63b5013f9b
Etsu <etsu4296@gmail.com>
2016-08-29 19:44:48 +0400

    change date format

--- vulpixCSV_SQL.sql
+++ vulpixCSV_SQL.sql
@@ -1,14 +1,11 @@
-LOAD DATA INFILE '/home/e7su/projects/vulpix/Vulpix.csv'
-INTO TABLE Vulpix
-FIELDS TERMINATED BY ';' ENCLOSED BY '"' ESCAPED BY '\\'
-LINES TERMINATED BY '\n'
-(cus,code_pu,pu,email,tel,@date_login_ak,@date_last_login_ak,@date_login_aml,
+LOAD DATA INFILE '/var/lib/mysql-files/Vulpix.csv' 
+INTO TABLE Vulpix FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' 
+(cus,code_mail,@pika,tel,@date_login_ak,@date_last_login_ak,@date_login_aml,
 @date_login_am,@date_last_login_aml,@date_last_login_am,actions,lite_full,
-ukd_chennel,@action_date,action_execution_chennel)
-SET date_login_ak = STR_TO_DATE(@date_login_ak, '%Y-%m-%d'),
-date_last_login_ak = STR_TO_DATE(@date_last_login_ak, '%Y-%m-%d'),
-date_login_aml = STR_TO_DATE(@date_login_aml, '%Y-%m-%d'),
-date_login_am = STR_TO_DATE(@date_login_am, '%Y-%m-%d'),
-date_last_login_aml = STR_TO_DATE(@date_last_login_aml, '%Y-%m-%d'),
-date_last_login_am = STR_TO_DATE(@date_last_login_am, '%Y-%m-%d'),
-action_date = STR_TO_DATE(@action_date, '%Y-%m-%d');
+ukd_chennel,@action_date,action_execution_chennel) 
+SET date_login_ak = STR_TO_DATE(replace(@date_login_ak,'NULL',''), '%Y-%m-%d %H:%i:%s.%x'),
+date_login_aml = STR_TO_DATE(replace(@date_login_aml,'NULL',''), '%Y-%m-%d %H:%i:%s.%x'),
+date_login_am = STR_TO_DATE(replace(@date_login_am,'NULL',''), '%Y-%m-%d %H:%i:%s.%x'),
+date_last_login_aml = STR_TO_DATE(replace(@date_last_login_aml,'NULL',''), '%Y-%m-%d %H:%i:%s.%x'),
+date_last_login_am = STR_TO_DATE(replace(@date_last_login_am,'NULL',''), '%Y-%m-%d %H:%i:%s.%x'),
+action_date = STR_TO_DATE(replace(@action_date,'NULL',''), '%Y-%m-%d %H:%i:%s.%x');
